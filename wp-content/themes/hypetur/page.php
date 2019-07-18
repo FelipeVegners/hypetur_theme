f32e366ef2fe114a6a254ed596bc44f77b6389da
FelipeVegners <felipevegners@gmail.com>
2019-07-18 10:58:02 -0300

    insert promo package id for anchor

--- wp-content/themes/hypetur/page.php
+++ wp-content/themes/hypetur/page.php
@@ -1,6 +1,6 @@
 <?php   get_header(); ?>
 
-<div class="promo-package" style="position:relative;">
+<div id="packages" class="promo-package" style="position:relative;">
   <?php
     $args = array(
     'post_type'	  => 'promo-package',
@@ -16,14 +16,14 @@
   ?>
   <div class="promo-package-wrapper">
     <section class="destination-section" style="background:url(<?php echo $package_meta['picture_url'][0] ?>) top center/cover no-repeat;">
-          <div class="destination-detail">
-            <h1><?php echo $package_meta['place'][0] ?></h1>
-            <small><?php echo $package_meta['country'][0] ?></small>
-            <p class="dest-period"><?php echo $package_meta['period'][0] ?></p>
-            <p class="mb-0"><?php echo $package_meta['intro'][0] ?></p>
-            <p><?php echo $package_meta['testmonial'][0] ?></p>
-            <button class="more-info-btn">Quero saber mais!</button>
-          </div>      
+      <div class="destination-detail">
+        <h1><?php echo $package_meta['place'][0] ?></h1>
+        <small><?php echo $package_meta['country'][0] ?></small>
+        <p class="dest-period"><?php echo $package_meta['period'][0] ?></p>
+        <p class="mb-0"><?php echo $package_meta['intro'][0] ?></p>
+        <p><?php echo $package_meta['testmonial'][0] ?></p>
+        <button class="more-info-btn">Quero saber mais!</button>
+      </div>      
     </section>
     <section class="destination-about">
       <div class="container">
@@ -36,19 +36,19 @@
         <div class="row destination-price">
           <div class="col-md-6">
             <div class="row justify-content-center">
-                <?php 
-                  $includes = $package_meta['package_includes'];
-                  foreach ( $includes as $include ) {
-                ?>
-                <div class="col-md-3 package-icon">
-                  <span class="icon-wrapper" data-icon="<?php echo $value; ?>">
-                    <img class="icon -<?php echo $include; ?>" src="<?php echo get_template_directory_uri();?>/images/icons/<?php echo $include; ?>.svg"> -->
-                  </span>
-                  <p class="icon-label"><?php echo $include; ?></p>
-                </div>
-                <?php
-                  }
-                ?>
+              <?php 
+                $includes = $package_meta['package_includes'];
+                foreach ( $includes as $include ) {
+              ?>
+              <div class="col-md-3 package-icon">
+                <span class="icon-wrapper" data-icon="<?php echo $value; ?>">
+                  <img class="icon -<?php echo $include; ?>" src="<?php echo get_template_directory_uri();?>/images/icons/<?php echo $include; ?>.svg"> -->
+                </span>
+                <p class="icon-label"><?php echo $include; ?></p>
+              </div>
+              <?php
+                }
+              ?>
             </div>
             <div class="row">
               <div class="col-md-12">
@@ -95,17 +95,17 @@
             <div class="package-more-info">
               <ul class="nav nav-tabs" id="package-more-details" role="tablist">
                 <li class="nav-item">
-                  <a class="nav-link active" id="home-tab" data-toggle="tab" href="#includes-<?php echo $package_meta['country'][0] ?>" role="tab" aria-controls="home" aria-selected="true">Inclusões</a>
+                  <a class="more-info-tab active" id="includes" data-toggle="tab" href="#includes-<?php echo $package_meta['country'][0] ?>" role="tab" aria-controls="home" aria-selected="true">Inclusões</a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" id="profile-tab" data-toggle="tab" href="#schedule-<?php echo $package_meta['country'][0] ?>" role="tab" aria-controls="profile" aria-selected="false">Roteiro Diário</a>
+                  <a class="more-info-tab" id="schedule" data-toggle="tab" href="#schedule-<?php echo $package_meta['country'][0] ?>" role="tab" aria-controls="profile" aria-selected="false">Roteiro Diário</a>
                 </li>
               </ul>
               <div class="tab-content">
-                <div class="tab-pane fade show active" id="includes-<?php echo $package_meta['country'][0] ?>" role="tabpanel" aria-labelledby="home-tab">
+                <div class="tab-pane fade show active" id="includes-<?php echo $package_meta['country'][0] ?>" role="tabpanel" aria-labelledby="includes">
                   <?php echo $package_meta['package_tab_inclusions'][0] ?>
                 </div>
-                <div class="tab-pane fade" id="schedule-<?php echo $package_meta['country'][0] ?>" role="tabpanel" aria-labelledby="profile-tab">
+                <div class="tab-pane fade" id="schedule-<?php echo $package_meta['country'][0] ?>" role="tabpanel" aria-labelledby="schedule">
                   <?php echo $package_meta['package_tab_schedule'][0] ?>
                 </div>
               </div>
