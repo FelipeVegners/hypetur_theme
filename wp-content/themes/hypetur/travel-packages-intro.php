9ca7974e4348679f7d290b5593cb7e61d23dee16
FelipeVegners <felipevegners@gmail.com>
2020-02-07 18:08:30 -0300

    packages intro image added

--- wp-content/themes/hypetur/travel-packages-intro.php
+++ wp-content/themes/hypetur/travel-packages-intro.php
@@ -2,41 +2,33 @@
   <div class="container">
     <h1 class="section-title">Pacotes de viagem</h1>
     <div class="col-md-12">
-      <div class="row py-4 justify-content-between">
+      <div class="row py-4 d-flex justify-content-between">
+      <!-- Get taxonomy images (needs Taxonomy Images plugin) -->
         <?php 
-          $args=array(
-            'public'   => true,
-            '_builtin' => false
-          );
-          $taxonomies= get_taxonomies($args); 
-          if ($taxonomies) {
-            foreach ($taxonomies  as $taxonomy) {
-            $terms = get_terms([
-              'taxonomy' => $taxonomy,
-              'hide_empty' => false,
-            ]);
-            foreach ($terms as $term) {
+          $terms = apply_filters('taxonomy-images-get-terms', '', array(
+            'taxonomy' => 'destino',
+          ));
+          if ( ! empty( $terms ) ) {
+            foreach ( (array) $terms as $term ) {
         ?>
-          <div class="card my-2 mb-4" style="width: 16rem;">
-            <img class="card-img-top" src="https://cdn.panrotas.com.br/portal-panrotas-statics/media-files-cache/252803/ef9aa137530cb829856cee10bf0fed05mickeymouse1988522960720/0,35,960,573/670,400,0.52/0/default.jpg" alt="Card image cap">
+          <div class="card">
+            <img class="card-img-top" src="<?php echo esc_url(wp_get_attachment_url( $term->image_id))?>" alt="<?php echo $term->name?>">
             <div class="card-body d-flex flex-column justify-content-between">
               <h5>
                 <a class="blog__post__title" href="/pacotes-de-viagem/<?php echo $term->slug ?>" title="<?php echo $term->name?>"><?php echo $term->name; ?></a>
               </h5>
               <p class="card-text"><?php echo $term->description; ?></p>
-              <p class="card-text"><?php echo $term->taxonomy_image_url; ?></p>
               <a href="/pacotes-de-viagem/<?php echo $term->slug ?>" class="view-all-packages">Ver os pacotes</a>
             </div>
           </div>
         <?php 
             }
           }
-        }  
         ?>
     </div>
   </div>
   <div class="row">
-    <div class="col-md-12">
+    <div class="col-md-12 text-center">
     <button type="button" class="show-full-post">
       <a href="./pacotes-de-viagem">Ver todos os pacotes</a>
     </button>
